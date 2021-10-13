99ffc9dabcc05fbc8cf506843a4fe78a6c632fdf
Nathan Horne <n.horne90@hotmail.co.uk>
2021-10-13 16:03:36 +0100

    completed Reverse digits test

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,9 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+  const numStr = number.toString();
+  const numArr = Array.from(numStr).map(Number);
+  const reversed = numArr.reverse();
+
+  return reversed;
+};
 
 module.exports = numberToReversedDigits;
